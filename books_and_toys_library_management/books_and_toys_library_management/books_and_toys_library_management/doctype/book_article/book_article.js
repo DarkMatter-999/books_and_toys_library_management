9b3ff5f07d44cb1ce9716b6bdfb7dd20e544d29b
DarkMatter-999 <darkmatter999official@gmail.com>
2024-06-21 15:48:17 +0530

    Added all the necessary doctypes

---
+++ books_and_toys_library_management/books_and_toys_library_management/books_and_toys_library_management/doctype/book_article/book_article.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, Lakshyajeet Singh Goyal and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Book Article", {
+// 	refresh(frm) {
+
+// 	},
+// });
